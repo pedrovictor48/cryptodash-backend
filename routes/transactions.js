ad2c9220d20e46206d8ada44cb8d58a006ad696d
Pedro Ferreira <pedrovictor48@live.com>
2022-08-27 01:16:21 -0300

    error handling

--- routes/transactions.js
+++ routes/transactions.js
@@ -11,21 +11,29 @@ async function createTransaction(req, res, next) {
 }
 
 async function deleteTransaction(req, res) {
-    const { transactionId } = req.body;
-    if (!transactionId) {
-        return res
-            .status(400)
-            .json({ message: "Transaction ID must be provided" });
-    }
+    try {
+        const { transactionId } = req.body;
+        if (!transactionId) {
+            return res
+                .status(400)
+                .json({ message: "Transaction ID must be provided" });
+        }
 
-    await transactionSchema.findByIdAndRemove(transactionId);
-    return res.status(200).json({ message: "Ok" });
+        await transactionSchema.findByIdAndRemove(transactionId);
+        return res.status(200).json({ message: "Ok" });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
 }
 
 async function getTransaction(req, res) {
-    userId = req.body.userId;
-    const transactions = await transactionSchema.find({ userId: userId });
-    return res.status(200).json({ message: "Ok", data: transactions });
+    try {
+        userId = req.body.userId;
+        const transactions = await transactionSchema.find({ userId: userId });
+        return res.status(200).json({ message: "Ok", data: transactions });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
 }
 
 module.exports = { createTransaction, getTransaction, deleteTransaction };
